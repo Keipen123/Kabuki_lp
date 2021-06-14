2314ee0cf4d39eb760478f01c786400a382bfdc4
Hiro <s4moozy@gmail.com>
2021-06-14 18:29:01 +0900

    Added NFT about section

---
+++ components/About/AboutItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="md:w-1/2 py-4 md:px-2">
+    <h5 class="text-red-600 font-bold text-2xl md:text-4xl">
+      <slot name="title"></slot>
+    </h5>
+    <div>
+      <slot name="img"></slot>
+    </div>
+    <div>
+      <slot name="content"></slot>
+    </div>
+  </li>
+</template>
+
+
+<style lang="postcss" scoped>
+  
+</style>
