93664ab8aa5e810d7882650fc7bf4406736784a8
Keipen123 <CBeTpenguin@gmail.com>
2021-07-01 19:48:01 +0900

    add social media icon

--- components/Social/SocialSection.vue
+++ components/Social/SocialSection.vue
@@ -1,6 +1,11 @@
 <template>
   <section>
-    test
+    <div class="box flex space-x-3">
+    <div><img src="@/assets/images/icon_medium128.png" class="w-auto h-12 md:h-14 lg:h-20"></div>
+    <div><img src="@/assets/images/icon_twitter128.png" class="w-auto h-12 md:h-14 lg:h-20"></div>
+    <div><img src="@/assets/images/icon_telegram128.png" class="w-auto h-12 md:h-14 lg:h-20"></div>
+  </div>
+
   </section>
 </template>
 
